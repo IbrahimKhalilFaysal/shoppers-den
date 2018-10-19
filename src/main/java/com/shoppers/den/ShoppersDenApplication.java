3e43d7ad70d08b6c531367456ea765eebd1bea2a
Ibrahim Khalil <faysal.sarkar@gmail.com>
2018-10-19 08:27:32 +0600

    First Commit - Project Skeleton

---
+++ src/main/java/com/shoppers/den/ShoppersDenApplication.java
@@ -0,0 +1,12 @@
+package com.shoppers.den;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppersDenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppersDenApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shoppers/den/ShoppersDenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shoppers.den;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShoppersDenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
